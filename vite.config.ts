f117afc17e5b57cc38591fe457ea31f5c462ebc0
unknown <dominik.zimny99@gmail.com>
2024-11-14 14:40:28 +0100

    config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/uni-courses/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
